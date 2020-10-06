d01070791a69dc8b58aaf92a4bd468eef18fcee5
Daniel <dbruer22@bsmschool.org>
2020-10-06 12:53:55 -0500

    adding all the streaming files to the depository

---
+++ mjpgstreamer6.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 60 -r 320x240 -d /dev/video4" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www "
---
+++ mjpgstreamertinker.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 60 -r 160x120 -d /dev/video0" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www "
