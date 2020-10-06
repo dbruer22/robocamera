b40ae59d27cd5c5fbc976c9742dcabef18442ee0
Daniel <dbruer22@bsmschool.org>
2020-10-06 12:57:42 -0500

    this will activate all the code

---
+++ activator.sh
@@ -0,0 +1,8 @@
+chmod +x mjpgstreamer1.sh
+chmod +x mjpgstreamer2.sh
+chmod +x mjpgstreamer3.sh
+chmod +x mjpgstreamer4.sh
+chmod +x mjpgstreamer5.sh
+chmod +x mjpgstreamer6.sh
+chmod +x mjpgstreamerstone.sh
+chmod +x mjpgstreamertinker.sh
