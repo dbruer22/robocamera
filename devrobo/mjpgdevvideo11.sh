fee7ae3ca5cd244f5fd6b381acd30577af2fcb47
dbruer22 <56086377+dbruer22@users.noreply.github.com>
2022-02-24 09:44:26 -0600

    Add files via upload

---
+++ devrobo/mjpgdevvideo11.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 24 -r 320x240 -d /dev/video11" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8175 -w /usr/local/share/mjpg-streamer/www "
---
+++ devrobo/mjpgdevvideo8.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/mjpg_streamer -i "/usr/local/lib/mjpg-streamer/input_uvc.so -n -f 24 -r 320x240 -d /dev/video8" -o "/usr/local/lib/mjpg-streamer/output_http.so -p 8065 -w /usr/local/share/mjpg-streamer/www "
